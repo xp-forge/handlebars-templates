0de156f6f05ce2badc7b123fe5cda612c17f0fbc
Timm Friebe <thekid@thekid.de>
2022-09-18 17:08:49 +0200

    Allow passing maps to extensions

---
+++ src/test/php/web/frontend/unittest/ExtensionsTest.class.php
@@ -0,0 +1,32 @@
+<?php namespace web\frontend\unittest;
+
+use unittest\{Assert, Test, Values};
+
+class ExtensionsTest extends HandlebarsTest {
+
+  /** @return web.frontend.Extension[] */
+  protected function extensions() {
+    return [[
+      'scalar'   => 'test',
+      'map'      => ['test' => 'works'],
+      'function' => function($in, $context, $options) {
+        return strtoupper($options[0]);
+      }
+    ]];
+  }
+
+  #[Test]
+  public function bound_scalar() {
+    Assert::equals('test', $this->transform('{{scalar}}'));
+  }
+
+  #[Test]
+  public function bound_map() {
+    Assert::equals('works', $this->transform('{{map.test}}'));
+  }
+
+  #[Test]
+  public function bound_function() {
+    Assert::equals('TEST', $this->transform('{{function "test"}}'));
+  }
+}
